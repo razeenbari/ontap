ab3e245943c2f20d183d22d8d6d79f3ed8540a9c
Razeen Bari <64928256+razeenbari@users.noreply.github.com>
2020-06-01 20:16:58 +0500

    Add files via upload

---
+++ ontap.com/app.js
@@ -0,0 +1,9 @@
+<script>
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+</script>
